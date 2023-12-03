5bd031e75e8d8dc98575e2a8ab4dbe7043aef51b
Brian Starkey <stark3y@gmail.com>
2023-12-03 13:13:12 +0000

    03: part2

--- 03/main.rs
+++ 03/main.rs
@@ -2,12 +2,14 @@ use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::option::Option;
 
+// Only returns the fist symbol found
 fn number_touches_symbol(
     num_coord: &(i32, i32),
     num: &String,
     symbol_coords: &HashMap::<(i32, i32), char>
-) -> bool {
+) -> Option<((i32, i32), char)> {
     let min_col = num_coord.0 - 1;
     let max_col = num_coord.0 + num.len() as i32;
     let min_row = num_coord.1 - 1;
@@ -17,13 +19,47 @@ fn number_touches_symbol(
         for col in min_col..=max_col {
             // This depends on none of the number's coordinates being
             // in symbol_coords, but why would they be?
-            if symbol_coords.contains_key(&(col, row)) {
-                return true;
+            match symbol_coords.get(&(col, row)) {
+                Some(c) => return Some(((col, row), *c)),
+                None => {},
             }
         }
     }
 
-    return false;
+    return None;
+}
+
+fn find_gears(
+    number_coords: &HashMap::<(i32, i32), String>,
+    symbol_coords: &HashMap::<(i32, i32), char>
+) -> Vec<(u32, u32)> {
+    let mut gear_candidates = HashMap::<(i32, i32), Vec<String>>::new();
+
+    for (coord, number) in number_coords.iter() {
+        // This assumes that a gear number _only_ touches the 
+        // gear. If it touches another symbol, then we might hit that
+        // first and number_touches_symbol will return that instead.
+        match number_touches_symbol(&coord, &number, symbol_coords) {
+            Some((symbol_coord, '*')) => {
+                gear_candidates.entry(symbol_coord)
+                    .or_insert_with(Vec::<String>::new)
+                    .push(number.to_string());
+            },
+            _ => (),
+        }
+    }
+
+    let mut gears = Vec::new();
+    for (_, numbers) in gear_candidates.iter() {
+        if numbers.len() == 2 {
+            gears.push((
+                    numbers[0].parse::<u32>().unwrap(),
+                    numbers[1].parse::<u32>().unwrap(),
+            ));
+        }
+    }
+
+    return gears;
 }
 
 fn main() -> io::Result<()> {
@@ -67,14 +103,23 @@ fn main() -> io::Result<()> {
     }
 
     let mut sum = 0;
-    for (coords, number) in number_coords.into_iter() {
-        let touches = number_touches_symbol(&coords, &number, &symbol_coords);
-        if touches {
-            sum += number.parse::<u32>().unwrap();
+    for (coords, number) in number_coords.iter() {
+        match number_touches_symbol(&coords, &number, &symbol_coords) {
+            Some(_) => {
+                sum += number.parse::<u32>().unwrap();
+            },
+            None => {},
         }
     }
 
     println!("{sum}");
+
+    let gear_sum: u32 = find_gears(&number_coords, &symbol_coords)
+        .iter()
+        .map(|g| { g.0 * g.1 })
+        .sum();
+
+    println!("{gear_sum}");
 
     Ok(())
 }

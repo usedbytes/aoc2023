bb911ab7f019f4363a835cd064f6c391720b476c
Brian Starkey <stark3y@gmail.com>
2023-12-08 21:02:39 +0000

    08: Part 2
    The first maths-puzzle-y one. Bit of a struggle, but we got there in the
    end.

--- 08/main.rs
+++ 08/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -29,6 +29,26 @@ fn run_route<'a>(
 
         options = map.get(node).unwrap();
     }
+}
+
+fn get_factors(n: u32) -> HashSet<u32> {
+    let mut i = 2;
+    let mut factors = HashSet::new();
+
+    while i <= n {
+        let div = n / i;
+        let rem = n % i;
+
+        if rem == 0 {
+            if !factors.insert(div) || !factors.insert(i) {
+                return factors;
+            }
+        }
+
+        i += 1;
+    }
+
+    return factors;
 }
 
 fn main() -> io::Result<()> {
@@ -67,17 +87,20 @@ fn main() -> io::Result<()> {
         );
     }
 
+    // Run Part 1 - the "sample3" input doesn't have an AAA so just guard this
+    // against that.
     if map.get("AAA").is_some() {
         let (moves, _) = run_route(&route, &map, "AAA", |n| n == "ZZZ");
         println!("Part 1: {:?}", moves);
     }
 
+    // Match function for ending Part 2
     fn ends_with_z(s: &String) -> bool {
         return (&s).bytes().last().is_some_and(|b| b == b'Z');
     }
 
+    // Find the route lengths for all starting positions.
     let mut route_lengths = Vec::new();
-
     for s in starts {
         let (moves, node) = run_route(&route, &map, &s, ends_with_z);
         let (moves2, node2) = run_route(&route, &map, node, ends_with_z);
@@ -90,7 +113,24 @@ fn main() -> io::Result<()> {
         route_lengths.push(moves);
     }
 
-    println!("Route Lengths: {:?}", &route_lengths);
+    // We need to find the least-common-multiple of all the routes, which we'll
+    // do by factorising. Presumably the input is crafted to give prime factors
+    let mut all_factors: HashSet<u32> = HashSet::new();
+    for l in &route_lengths {
+        let factors = get_factors(*l as u32);
+        all_factors.extend(factors.iter());
+    }
+
+    // We must have all prime factors, otherwise more work is needed
+    // to find lcm
+    assert!(all_factors.len() == route_lengths.len() + 1);
+
+    let mut product: u64 = 1;
+    for f in &all_factors {
+        product *= *f as u64;
+    }
+
+    println!("Part 2: {}", product);
 
     Ok(())
 }

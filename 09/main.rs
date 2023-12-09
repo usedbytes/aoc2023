32c20526b963b447c71b6fbed82a63e3293d7257
Brian Starkey <stark3y@gmail.com>
2023-12-09 10:06:22 +0000

    09: Part 2: Finally an easy day!

--- 09/main.rs
+++ 09/main.rs
@@ -12,15 +12,16 @@ fn calc_diff(vals: &Vec<i32>) -> Vec<i32> {
     return result;
 }
 
-fn extrapolate(vals: &Vec<i32>) -> i32 {
+fn extrapolate(vals: &Vec<i32>, start: bool) -> (i32, i32) {
     let diff = calc_diff(vals);
     if diff.iter().all(|v| *v == 0) {
-        return vals[vals.len() - 1];
+        // Doesn't actually matter which we return
+        return (vals[0], vals[vals.len()-1]);
     }
 
-    let result =  vals[vals.len()-1] + extrapolate(&diff);
+    let (ex_start, ex_end) = extrapolate(&diff, start);
 
-    return result;
+    return (vals[0] - ex_start, vals[vals.len()-1] + ex_end);
 }
 
 fn main() -> io::Result<()> {
@@ -30,7 +31,8 @@ fn main() -> io::Result<()> {
     let file = File::open(fname)?;
     let reader = io::BufReader::new(file);
 
-    let mut sum = 0;
+    let mut part1 = 0;
+    let mut part2 = 0;
     for line in reader.lines() {
         let line = line.unwrap();
 
@@ -39,12 +41,13 @@ fn main() -> io::Result<()> {
             .map(|v| v.parse::<i32>().unwrap())
             .collect();
 
-        let next = extrapolate(&vals);
-
-        sum += next;
+        let (start, end) = extrapolate(&vals, false);
+        part1 += end;
+        part2 += start;
     }
 
-    println!("{}", sum);
+    println!("{}", part1);
+    println!("{}", part2);
 
     Ok(())
 }

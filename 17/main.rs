1c7f2446a844efe775f8d736a0e155596b47a5f2
Brian Starkey <stark3y@gmail.com>
2023-12-17 22:31:22 +0000

    17: Part 1: Need to seed search with East and South.
    I guess my input's shortest paths all start with "South"

--- 17/main.rs
+++ 17/main.rs
@@ -12,7 +12,7 @@ const DIRS: [(i32, i32); 4] = [
     (0, -1),
 ];
 
-fn get_next(
+fn move_in_dir(
     map: &Vec<Vec<u8>>,
     from: &(usize, usize),
     dir: usize
@@ -29,59 +29,8 @@ fn get_next(
     return Some((nx as usize, ny as usize));
 }
 
-fn explore(
-    map: &Vec<Vec<u8>>,
-    cheapest: &mut BTreeMap::<(usize, usize), u32>,
-    from: &(usize, usize),
-    cost: u32,
-    dir: usize,
-    straight: u32,
-    depth: usize) {
-
-    let indent = "   ".repeat(depth);
-    println!("{indent} {:?}, {}, {}, {}", from, straight, dir, cost);
-
-    if let Some(current_cost) = cheapest.get_mut(from) {
-        if cost >= *current_cost {
-            println!("Already cheaper: {}", current_cost);
-            return;
-        }
-    }
-
-    cheapest.insert(*from, cost);
-
-    { // Turn right
-        let nd = ((dir as i32 + 1).rem_euclid(4)) as usize;
-        println!("nd: {nd}");
-        if let Some(next) = get_next(map, from, nd) {
-            let next_cost = cost + map[from.1][from.0] as u32;
-            explore(map, cheapest, &next, next_cost, nd, 0, depth + 1);
-        }
-    }
-
-    { // Turn left
-        let nd = ((dir as i32 - 1).rem_euclid(4)) as usize;
-        println!("nd: {nd}");
-        if let Some(next) = get_next(map, from, nd) {
-            let next_cost = cost + map[from.1][from.0] as u32;
-            explore(map, cheapest, &next, next_cost, nd, 0, depth + 1);
-        }
-    }
-
-    if straight < 2 { // Turn left
-        let nd = dir;
-        if let Some(next) = get_next(map, from, nd) {
-            let next_cost = cost + map[from.1][from.0] as u32;
-            explore(map, cheapest, &next, next_cost, nd, straight + 1, depth + 1);
-        }
-    }
-}
-
-// (x, y), cost, dir, straight
-type State = ((usize, usize), u32, usize, usize);
-
 #[derive(Copy, Clone, Eq, PartialEq)]
-struct State2 {
+struct State {
     pos: (usize, usize),
     cost: u32,
     dir: usize,
@@ -89,89 +38,95 @@ struct State2 {
 }
 
 // This is materially the same as the binary_heap example in the docs
-impl Ord for State2 {
+impl Ord for State {
     fn cmp(&self, other: &Self) -> Ordering {
         other.cost.cmp(&self.cost)
             .then_with(|| self.pos.cmp(&other.pos))
-            .then_with(|| other.straight.cmp(&self.straight))
     }
 }
 
-impl PartialOrd for State2 {
+impl PartialOrd for State {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-fn explore2(
+fn explore(
     map: &Vec<Vec<u8>>,
-    cheapest: &mut BTreeMap::<(usize, usize, usize), u32>,
-) {
-    //let mut to_explore: Vec<State> = Vec::new();
+) -> u32 {
     let mut to_explore = BinaryHeap::new();
 
-    let first_state = State2{
-        pos: (0, 0),
-        cost: 0,
-        dir: 0,
-        straight: 0
-    };
-    to_explore.push(first_state);
+    // x, y, dir, straight
+    let mut cheapest: BTreeMap<(usize, usize, usize, usize), u32> = BTreeMap::new();
 
     let goal = (map[0].len() - 1, map.len() - 1);
 
-    while let Some(State2{ pos, cost, dir, straight }) = to_explore.pop() {
+    // Need to seed the search with both East and South
+    to_explore.push(State{
+        pos: (0, 0),
+        cost: 0,
+        dir: 0,
+        straight: 0,
+    });
+
+    to_explore.push(State{
+        pos: (0, 0),
+        cost: 0,
+        dir: 1,
+        straight: 0,
+    });
+
+    while let Some(State{ pos, cost, dir, straight }) = to_explore.pop() {
 
         //println!("{:?}, {}, {}, {}", pos, cost, dir, straight);
 
-        if let Some(current_cost) = cheapest.get(&(pos.0, pos.1, straight)) {
+        if let Some(current_cost) = cheapest.get(&(pos.0, pos.1, dir, straight)) {
             if cost >= *current_cost {
                 //println!("Already cheaper: {}", current_cost);
                 continue;
             }
         }
 
-        cheapest.insert((pos.0, pos.1, straight), cost);
+        cheapest.insert((pos.0, pos.1, dir, straight), cost);
 
         if pos == goal {
-            println!("goal in {}", cost);
+            return cost;
         }
 
         { // Turn right
             let nd = ((dir as i32 + 1).rem_euclid(4)) as usize;
-            if let Some(next) = get_next(map, &pos, nd) {
+            if let Some(next) = move_in_dir(map, &pos, nd) {
                 let next_cost = cost + map[next.1][next.0] as u32;
                 //println!("right next: {:?}, {}", next, next_cost);
-                to_explore.push(State2{
+                to_explore.push(State{
                     pos: next,
                     cost: next_cost,
                     dir: nd,
-                    straight: 0
+                    straight: 0,
                 });
             }
         }
 
         { // Turn left
             let nd = ((dir as i32 - 1).rem_euclid(4)) as usize;
-            if let Some(next) = get_next(map, &pos, nd) {
+            if let Some(next) = move_in_dir(map, &pos, nd) {
                 let next_cost = cost + map[next.1][next.0] as u32;
                 //println!("left next: {:?}, {}", next, next_cost);
-                to_explore.push(State2{
+                to_explore.push(State{
                     pos: next,
                     cost: next_cost,
                     dir: nd,
-                    straight: 0
+                    straight: 0,
                 });
             }
         }
 
         if straight < 2 { // Straight
             let nd = dir;
-            if let Some(next) = get_next(map, &pos, nd) {
+            if let Some(next) = move_in_dir(map, &pos, nd) {
                 let next_cost = cost + map[next.1][next.0] as u32;
                 //println!("straight next: {:?}, {}", next, next_cost);
-                //to_explore.push((next, next_cost, nd, straight + 1));
-                to_explore.push(State2{
+                to_explore.push(State{
                     pos: next,
                     cost: next_cost,
                     dir: nd,
@@ -180,6 +135,8 @@ fn explore2(
             }
         }
     }
+
+    unreachable!();
 }
 
 fn main() -> io::Result<()> {
@@ -200,17 +157,8 @@ fn main() -> io::Result<()> {
         map.push(row);
     }
 
-    println!("{:?}", map);
-
-    let mut cheapest: BTreeMap<(usize, usize, usize), u32> = BTreeMap::new();
-
-    //explore(&map, &mut hottest, &(0, 0), 0, 0, 0, 0);
-    explore2(&map, &mut cheapest);
-
-    println!("{:?}", cheapest.entry((map.len() - 1, map[0].len() - 1, 0)));
-    println!("{:?}", cheapest.entry((map.len() - 1, map[0].len() - 1, 1)));
-    println!("{:?}", cheapest.entry((map.len() - 1, map[0].len() - 1, 2)));
-    println!("{:?}", cheapest.entry((map.len() - 1, map[0].len() - 1, 3)));
+    let cost = explore(&map);
+    println!("{cost}");
 
     Ok(())
 }

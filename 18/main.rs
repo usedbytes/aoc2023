44d2a458f855d103c49ab98de17e1e49d11aa742
Brian Starkey <stark3y@gmail.com>
2023-12-18 11:36:58 +0000

    18: P1 + P2 common algorithm
    Use the polygon area for both parts.

--- 18/main.rs
+++ 18/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs::File;
-use std::collections::{BTreeMap, HashMap};
+use std::collections::HashMap;
 use std::io::{self, BufRead};
 
 const DIRS: [(i32, i32); 4] = [
@@ -9,31 +9,6 @@ const DIRS: [(i32, i32); 4] = [
     (-1, 0),
     (0, -1),
 ];
-
-fn flood_fill(
-    perimeter: &BTreeMap<(i32, i32), u32>,
-    start: &(i32, i32),
-) -> usize {
-    let mut trench = perimeter.clone();
-
-    let mut search = Vec::new();
-    search.push(*start);
-
-    while let Some(pos) = search.pop() {
-        trench.insert(pos, 0);
-
-        for i in 0..4 {
-            let dp = DIRS[i];
-            let next_pos = (pos.0 + dp.0, pos.1 + dp.1);
-
-            if let None = trench.get(&next_pos) {
-                search.push(next_pos);
-            }
-        }
-    }
-
-    return trench.len();
-}
 
 fn polygon_area(verts: &Vec<(i32, i32)>) -> i64 {
     let mut area: i64 = 0;
@@ -73,66 +48,47 @@ fn main() -> io::Result<()> {
         ("R", (1, 0)),
     ]);
 
-    let mut perimeter: BTreeMap<(i32, i32), u32> = BTreeMap::new();
-    let mut pos: (i32, i32) = (0, 0);
-    let mut pos2: (i32, i32) = (0, 0);
+    let mut p1_verts = Vec::new();
+    let mut p2_verts = Vec::new();
 
-    let mut max = (0, 0);
-    let mut min = (0, 0);
-
-    let mut verts = Vec::new();
-    verts.push(pos2);
+    p1_verts.push((0, 0));
+    p2_verts.push((0, 0));
 
     for line in reader.lines() {
         let line = line?;
         let parts: Vec<_> = line.split(" ").collect();
 
-        let dp = dig_dirs.get(parts[0]).unwrap();
-        let n = parts[1].parse::<i32>().unwrap();
-        let color = u32::from_str_radix(&parts[2][2..8], 16).unwrap();
+        { // Part 1
+            let dp = dig_dirs.get(parts[0]).unwrap();
+            let n = parts[1].parse::<i32>().unwrap();
+            let last = p1_verts.last().unwrap();
 
-        for _ in 1..=n {
-            pos.0 += dp.0;
-            pos.1 += dp.1;
-            perimeter.insert(pos, color);
-
-            min.0 = std::cmp::min(min.0, pos.0);
-            min.1 = std::cmp::min(min.1, pos.1);
-            max.0 = std::cmp::max(max.0, pos.0);
-            max.1 = std::cmp::max(max.1, pos.1);
+            p1_verts.push((
+                last.0 + dp.0 * n,
+                last.1 + dp.1 * n,
+            ));
         }
 
-        let dir2 = DIRS[(color & 0xf) as usize];
-        let n2 = (color >> 4) as i32;
-        pos2 = (
-            pos2.0 + dir2.0 * n2,
-            pos2.1 + dir2.1 * n2,
-        );
-        verts.push(pos2);
+        { // Part 2
+            let insn = u32::from_str_radix(&parts[2][2..8], 16).unwrap();
+            let dp = DIRS[(insn & 0xf) as usize];
+            let n = (insn >> 4) as i32;
+            let last = p2_verts.last().unwrap();
+
+            p2_verts.push((
+                last.0 + dp.0 * n,
+                last.1 + dp.1 * n,
+            ));
+        }
     }
 
-    // FIXME: Assumes (1, 1) is inside, which might not be true.
-    let start = (1, 1);
-
-    let size = flood_fill(&perimeter, &start);
-    println!("{size}");
-
-    let area = polygon_area(&verts);
-    let perimeter = polygon_perimeter(&verts);
+    let area = polygon_area(&p1_verts);
+    let perimeter = polygon_perimeter(&p1_verts);
     println!("{}", area + perimeter / 2 + 1);
 
-    /*
-    for row in min.1..=max.1 {
-        for col in min.0..=max.0 {
-            if let Some(_) = perimeter.get(&(col, row)) {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-        println!("");
-    }
-    */
+    let area = polygon_area(&p2_verts);
+    let perimeter = polygon_perimeter(&p2_verts);
+    println!("{}", area + perimeter / 2 + 1);
 
     Ok(())
 }

3d613ac4b11bd2973b9f6c7bf32ecc89dc0dc939
Brian Starkey <stark3y@gmail.com>
2023-12-23 17:58:55 +0000

    23: Part 2 - Still slow!
    Implement a new method which builds a graph rather than searching the
    map over and over again. This is still super slow, but it gets the
    answer... Debug build ~2m30s on my M1, release ~7s.

--- 23/main.rs
+++ 23/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::error::Error;
 use std::io::{self, BufRead};
-use std::collections::{HashSet, HashMap};
+use std::collections::{BTreeSet, BTreeMap};
 use std::fs::File;
 
 const DIRS: [(i32, i32); 4] = [
@@ -14,7 +14,8 @@ const DIRS: [(i32, i32); 4] = [
 fn move_in_dir(
     map: &Vec<Vec<char>>,
     from: &(usize, usize),
-    dir: usize
+    dir: usize,
+    part2: &bool
 ) -> Option<(usize, usize)> {
     let dp = DIRS[dir];
     let nx = from.0 as i32 + dp.0;
@@ -25,32 +26,48 @@ fn move_in_dir(
         return None;
     }
 
-    if map[ny as usize][nx as usize] == '#' {
-        return None;
-    }
+    let nx = nx as usize;
+    let ny = ny as usize;
 
-    return Some((nx as usize, ny as usize));
+    let cell = map[ny][nx];
+    match (part2, cell, dir) {
+        (_, '#', _) => None,
+        (false, '>', 2) => None,
+        (false, 'v', 3) => None,
+        (false, '<', 0) => None,
+        (false, '^', 1) => None,
+        (false, '.', _) => Some((nx, ny)),
+        _ => Some((nx, ny)),
+    }
 }
 
-fn explore(
+type Graph = BTreeMap<(usize, usize), BTreeSet<((usize, usize), usize)>>;
+
+fn insert_edge(graph: &mut Graph, a: &(usize, usize), b: &(usize, usize), distance: usize, bidir: &bool) {
+    graph.entry(*a)
+        .or_insert(BTreeSet::new())
+        .insert((*b, distance));
+    if *bidir {
+        graph.entry(*b)
+            .or_insert(BTreeSet::new())
+            .insert((*a, distance));
+    }
+}
+
+fn build_graph(
     map: &Vec<Vec<char>>,
+    graph: &mut Graph,
     from: &(usize, usize),
-    goal: &(usize, usize),
-    path: &HashSet<(usize, usize)>,
-    prev_cell: char,
-    part2: &bool) -> Vec<usize> {
+    fork: &(usize, usize),
+    forks: &mut BTreeSet<(usize, usize)>,
+    part2: &bool) {
 
     let mut current = *from;
-    let mut path = path.clone();
-    let start_n = path.len();
+    let mut prev = *fork;
+
+    let mut n = if *from == *fork { 0 } else { 1 };
 
     loop {
-        path.insert(current.clone());
-
-        if current == *goal {
-            return vec![path.len() - 1];
-        }
-
         let cell = map[current.1][current.0];
         let dirs = match (part2, cell) {
             (false, '>') => 0..=0,
@@ -65,24 +82,67 @@ fn explore(
 
         let options: Vec<(usize, usize)> = dirs.iter()
             .filter_map(
-                |d| move_in_dir(map, &current, *d)
+                |d| move_in_dir(map, &current, *d, part2)
             )
-            .filter(|v| !path.contains(v))
+            .filter(|v| *v != prev) // Can't go back - this only works if the path is 1-wide!
             .collect();
 
         if options.len() > 1 {
-            let mut results = Vec::new();
-            for option in options {
-                let lengths = explore(map, &option, goal, &path, prev_cell, part2);
-                results.extend(lengths);
+            insert_edge(graph, fork, &current, n, part2);
+
+            if !forks.contains(&current) {
+                forks.insert(current);
+                for option in options {
+                    build_graph(map, graph, &option, &current, forks, part2);
+                }
             }
-            return results;
+
+            return;
         } else if options.len() == 0 {
-            return vec![];
+            insert_edge(graph, fork, &current, n, part2);
+
+            return;
         } else {
+            n += 1;
+            prev = current;
             current = options[0];
         }
     }
+}
+
+fn explore_graph(
+    graph: &Graph,
+    from: &(usize, usize),
+    goal: &(usize, usize),
+    path: &mut BTreeSet<(usize, usize)>,
+    distance: usize,
+    results: &mut BTreeSet<usize>,
+) {
+    if let Some(options) = graph.get(from) {
+        for (option, dist) in options {
+            if path.contains(&option) {
+                continue;
+            }
+
+            if option == goal {
+                results.insert(distance + dist);
+            } else {
+                let mut path = path.clone();
+                path.insert(*option);
+                explore_graph(graph, option, goal, &mut path, distance + dist, results);
+            }
+        }
+    }
+}
+
+fn graph_to_dot(graph: &Graph) {
+    println!("digraph G {{");
+    for (k, v) in graph {
+        for (dest, dist) in v {
+            println!("\"{:?}\" -> \"{:?}\"", k, dest);
+        }
+    }
+    println!("}}");
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -117,11 +177,25 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
     let end = (end_x, map.len() - 1);
 
-    let paths = explore(&map, &start, &end, &HashSet::new(), '.', &false);
-    println!("{:?}", paths.iter().max().unwrap());
+    // Part 1
+    {
+        let mut graph = Graph::new();
+        build_graph(&map, &mut graph, &start, &start, &mut BTreeSet::new(), &false);
 
-    let paths = explore(&map, &start, &end, &HashSet::new(), '.', &true);
-    println!("{:?}", paths.iter().max().unwrap());
+        let mut results = BTreeSet::new();
+        explore_graph(&graph, &start, &end, &mut BTreeSet::from([start]), 0, &mut results);
+        println!("{:?}", results.iter().max().unwrap());
+    }
+
+    // Part 2
+    {
+        let mut graph = Graph::new();
+        build_graph(&map, &mut graph, &start, &start, &mut BTreeSet::new(), &true);
+
+        let mut results = BTreeSet::new();
+        explore_graph(&graph, &start, &end, &mut BTreeSet::from([start]), 0, &mut results);
+        println!("{:?}", results.iter().max().unwrap());
+    }
 
     Ok(())
 }
